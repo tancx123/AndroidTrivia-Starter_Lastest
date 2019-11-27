2aa2d737378cbc4234d33d9fd27dddee0341e0f8
taruc <tancx-pm17@student.tarc.edu.my>
2019-11-27 09:49:19 +0800

    Lastest Commit

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -101,15 +102,19 @@ class GameFragment : Fragment() {
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
                         view.findNavController()
-                                .navigate(R.id.action_gameFragment_to_gameWonFragment)
+                                .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
                     view.findNavController()
-                            .navigate(R.id.action_gameFragment_to_gameOverFragment2)
+                            .navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment2())
                 }
             }
         }
+
+        //val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        //Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
         return binding.root
     }
 
